83b7774c2cea186b21a6086def53120f7e16b1b5
Peter Pan <apppeterpan@gmail.com>
2025-09-09 11:36:58 +0800

    Initial Commit

---
+++ CreatingMyFirstAppWithAISwiftUI242/CreatingMyFirstAppWithAISwiftUI242App.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingMyFirstAppWithAISwiftUI242App.swift
+//  CreatingMyFirstAppWithAISwiftUI242
+//
+//  Created by SHIH-YING PAN on 2025/9/9.
+//
+
+import SwiftUI
+
+@main
+struct CreatingMyFirstAppWithAISwiftUI242App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
